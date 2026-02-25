03b8883607a62649298faaff0347799dd3b33c54
Adrian Shuh-Humphries <ashuhhum@ucsc.edu>
2026-02-25 12:14:04 -0800

    rename gaddis to ch, ch2 c3-4

---
+++ ch2_c3_coutmozart.cpp
@@ -0,0 +1,11 @@
+// the Works of Wolfgang
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "The works of Wolfgang\ninclude the following";
+    cout << "\nThe Turkish March" << endl;
+    cout << "and symphony No. 40 ";
+    cout << "in G minor. ";
+    return 0;
+}
---
+++ ch2_c4_coutinfo.cpp
@@ -0,0 +1,9 @@
+//Today's Date
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Adrian\nStreet\nCity, State, 12345\n1234567890";
+    return 0;
+}
